94173a1aa3ec779b29278d4808653ce8b2194b29
Kelly Selden <kellyselden@gmail.com>
2015-05-08 16:59:12 -0400

    0.0.1

---
+++ TestHelper/AssertHelper.cs
@@ -0,0 +1,13 @@
+﻿using DeepEqual.Syntax;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHelper
+{
+	public static class AssertHelper
+	{
+		public static void AreDeeplyEqual(object expected, object actual)
+		{
+			Assert.IsTrue(expected.IsDeepEqual(actual));
+		}
+	}
+}
