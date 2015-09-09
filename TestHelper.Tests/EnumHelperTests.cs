90844dab28b57de59e338b7b56500963cd02c44e
Kelly Selden <kellyselden@gmail.com>
2015-09-09 15:37:03 -0400

    adding enum helpers

---
+++ TestHelper.Tests/EnumHelperTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestHelper.Tests
+{
+	[TestClass]
+	public class EnumHelperTests
+	{
+		enum Test { Test1 }
+
+		[TestMethod]
+		public void TestMethod1()
+		{
+			var actual = EnumHelper.ToEnumerable<Test>().ToArray();
+
+			Assert.AreEqual(1, actual.Length);
+			Assert.AreEqual(Test.Test1, actual.ElementAt(0));
+		}
+	}
+}
---
+++ TestHelper/EnumHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestHelper
+{
+	public static class EnumHelper
+	{
+		public static IEnumerable<T> ToEnumerable<T>()
+		{
+			return from object e in Enum.GetValues(typeof(T)) select (T)e;
+		}
+	}
+}
